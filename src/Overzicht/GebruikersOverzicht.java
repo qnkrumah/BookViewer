0ae0e629deca16f89467598b8bbcdf25f3a0eb75
qnkrumah <quincynkrumah@gmail.com>
2021-06-04 22:49:17 +0200

    Tweede main class aangemaakt en een de gebruikers en boeken gescheiden.
    Overzicht voor de Gebruikers aangemaakt waar enkel de developers toegang tot hebben

---
+++ src/Overzicht/GebruikersOverzicht.java
@@ -0,0 +1,48 @@
+package Overzicht;
+
+import User.GastAccount;
+import User.Gebruiker;
+import User.GebruikerAccount;
+
+import java.util.ArrayList;
+
+public class GebruikersOverzicht {
+
+    private ArrayList<User.Gebruiker> gebruikersLijst = new ArrayList<>();
+    private ArrayList<User.GastAccount> gastaccountLijst = new ArrayList<>();
+    private ArrayList<User.GebruikerAccount> gebruikersAccountlijst = new ArrayList<>();
+
+    public GebruikersOverzicht(){
+    }
+
+    public ArrayList<Gebruiker> getGebruikersLijst() {
+        return gebruikersLijst;
+    }
+
+    public void setGebruikersLijst(ArrayList<Gebruiker> gebruikersLijst) {
+        this.gebruikersLijst = gebruikersLijst;
+    }
+
+    public ArrayList<GastAccount> getGastaccountLijst() {
+        return gastaccountLijst;
+    }
+
+    public void setGastaccountLijst(ArrayList<GastAccount> gastaccountLijst) {
+        this.gastaccountLijst = gastaccountLijst;
+    }
+
+    public ArrayList<GebruikerAccount> getGebruikersAccountlijst() {
+        return gebruikersAccountlijst;
+    }
+
+    public void setGebruikersAccountlijst(ArrayList<GebruikerAccount> gebruikersAccountlijst) {
+        this.gebruikersAccountlijst = gebruikersAccountlijst;
+    }
+
+    @Override
+    public String toString() {
+        return "Overzicht{" +
+                "boekenLijst=" + gebruikersLijst +
+                '}';
+    }
+}
